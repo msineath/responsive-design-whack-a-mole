dca6d92fdb985c5f458d87449c1c2bf6eac19ecb
nasanov <asanoff.n@gmail.com>
2021-03-10 12:01:39 -0600

    poping up moles

--- mole.js
+++ mole.js
@@ -1,10 +1,23 @@
 // Write your JS here.
 window.addEventListener('DOMContentLoaded', () => {
 
-    setInterval(() => {
-        const moleHeads = document.querySelectorAll('.wgs__mole-head');
-        for(let moleHead of moleHeads) {
-            moleHead.classList.toggle('wgs__mole-head--hidden');
-        }
-    }, 1000);
+		const moleHeads = document.querySelectorAll('.wgs__mole-head');
+    // setInterval(() => {
+    //     for(let moleHead of moleHeads) {
+    //         moleHead.classList.toggle('wgs__mole-head--hidden');
+    //     }
+    // }, 1000);
+
+		function popUpRandomMole() {
+			let randomNum = Math.floor(Math.random() * 7);
+			let current = 	moleHeads[randomNum];
+			current.classList.remove('wgs__mole-head-hidden');
+			setTimeout(hideMole(current), 1000);
+		}
+		function hideMole(mole) {
+			mole.classList.add('wgs__mole-head--hidden');
+			setTimeout(popUpRandomMole, 1000);
+		}
+
+		setTimeout(popUpRandomMole, 0);
 })
