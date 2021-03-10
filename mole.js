ffad821561996dcdbf473b5c50714537714b5ba9
msineath <msineath@LAPTOP-65OGSQM1.localdomain>
2021-03-10 13:25:59 -0500

    working on whacked class

--- mole.js
+++ mole.js
@@ -7,17 +7,23 @@ window.addEventListener('DOMContentLoaded', () => {
     //         moleHead.classList.toggle('wgs__mole-head--hidden');
     //     }
     // }, 1000);
-
-		function popUpRandomMole() {
-			let randomNum = Math.floor(Math.random() * 7);
-			let current = 	moleHeads[randomNum];
-			current.classList.remove('wgs__mole-head-hidden');
-			setTimeout(hideMole(current), 1000);
-		}
-		function hideMole(mole) {
+    
+    function popUpRandomMole() {
+        let randomNum = Math.floor(Math.random() * 7);
+        let current = 	moleHeads[randomNum];
+        current.classList.remove('wgs__mole-head--hidden');
+        setTimeout( hideMole, 3000, current);
+    }
+    function hideMole(mole) {
+            console.log(mole);
 			mole.classList.add('wgs__mole-head--hidden');
 			setTimeout(popUpRandomMole, 1000);
 		}
+        
+        setTimeout(popUpRandomMole, 0);
 
-		setTimeout(popUpRandomMole, 0);
+    moleHeads.forEach(mole => mole.addEventListener('click', event => {
+        event.target.classList.add("wgs__mole-head--hidden");
+        event.target.classList.add("wgs__mole-head--whacked")
+    }))
 })
